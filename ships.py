8462cac06f1b8719a1d4db51a4e4aaf0cc5ce639
Travis <tjbailey10@gmail.com>
2016-10-25 15:17:11 -0400

    Project start
    added ship class

---
+++ ships.py
@@ -0,0 +1,13 @@
+class Ship:
+
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+        self.hit_points = size
+
+    def sunk(self):
+        if self.hit_points <= 0:
+            return True
+
+    def damage(self):
+        self.hit_points -= 1
